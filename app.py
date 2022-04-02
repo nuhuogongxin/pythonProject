c0b8f4c88fa77779180c1ce65d1d9ee75ff31a2e
fang <812472953@qq.com>
2022-04-02 15:22:36 +0800

    "Add database support with Flask-SQLAlchemy"

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask,render_template
 from flask import escape,url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+import click
+
+
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
 
 name = 'Grey Li'
 movies = [
@@ -18,7 +39,9 @@ movies = [
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user=User.query.first()
+    movies=Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -35,3 +58,14 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
